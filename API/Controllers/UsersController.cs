5167a4910f7a3258e618a51159512883cd7c3270
andrew-nazmy <eng.andrew.george711@gmail.com>
2023-05-10 21:25:06 +0300

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _Context;
+        public UsersController(DataContext context)
+        {
+            _Context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _Context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+
+            return await _Context.Users.FindAsync(id);
+
+        }
+    }
+}
